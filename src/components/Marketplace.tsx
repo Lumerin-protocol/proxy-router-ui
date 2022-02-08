d1abd31b53fb3286fc515899b50f01b2a406e529
Scott Millner <scottmillner237@gmail.com>
2022-02-07 17:13:58 -0700

    turnoff pagination for Marketplace table

--- src/components/Marketplace.tsx
+++ src/components/Marketplace.tsx
@@ -98,7 +98,7 @@ export const Marketplace: React.FC<MarketplaceProps> = ({ web3, contracts, setCo
 	);
 
 	const data = useMemo(() => getTableData(), [contracts, isLargeBreakpointOrGreater]);
-	const tableInstance = useTable<CustomTableOptions>({ columns, data, sortTypes, initialState: { pageSize: 5 } }, useSortBy, usePagination);
+	const tableInstance = useTable<CustomTableOptions>({ columns, data, sortTypes }, useSortBy);
 
 	// Remove spinner if no contracts after 1 minute
 	useInterval(() => {
@@ -108,7 +108,7 @@ export const Marketplace: React.FC<MarketplaceProps> = ({ web3, contracts, setCo
 	// There is always 1 empty contract for styling purposes
 	return (
 		<div className='flex flex-col items-center'>
-			{data.length > 1 ? <Table id='marketplace' tableInstance={tableInstance} columnCount={6} pagination /> : null}
+			{data.length > 1 ? <Table id='marketplace' tableInstance={tableInstance} columnCount={6} /> : null}
 			{data.length === 1 && showSpinner ? (
 				<div className='spinner'>
 					<Spinner />
