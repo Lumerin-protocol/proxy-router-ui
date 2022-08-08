87623d37c0db7247cc79f7507739e0ea28489b20
alan sorber <alan@titan.io>
2022-08-08 10:02:16 -0400

    update DEV deployedNetwork

--- src/web3/helpers.ts
+++ src/web3/helpers.ts
@@ -77,11 +77,9 @@ export const getWeb3ResultAsync: (
       else await (provider as WalletConnectProvider).enable();
       const web3 = new Web3(provider as provider);
       // const networkId = await web3.eth.net.getId();
-      //const deployedNetwork = (CloneFactory as ContractJson).networks[networkId]; // temp comment for testing 6/21 - MAY
-      //const deployedNetwork = {address: '0x702B0b76235b1DAc489094184B7790cAA9A39Aa4'}; //prod-beta 7/14/2022 - ABS
-      const deployedNetwork = {
-        address: "0x1F96Ac8f1a030aa0619ab9e203b37a7c942EEFe8",
-      }; //Dev-stg-2nd time testing 7/14/2022 -ABS
+      const deployedNetwork = {address: '0x1F96Ac8f1a030aa0619ab9e203b37a7c942EEFe8'}; //DEV 8/8/2022 -ABS
+      //const deployedNetwork = {address: '0x702B0b76235b1DAc489094184B7790cAA9A39Aa4'}; //STG 8/8/2022 -ABS
+      //const deployedNetwork = {address: '0x8C9C79Da3bbE8E1499a27cF56746Bb12cA83a2b9'}; //MAIN 8/8/2022 -ABS
       const accounts = await web3.eth.getAccounts();
       if (accounts.length === 0 || accounts[0] === "") {
         setAlertOpen(true);
