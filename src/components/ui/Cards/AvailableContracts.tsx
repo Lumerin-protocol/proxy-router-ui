deebbff415c0fb372926b31f13df621f6477bbec
codywall <cody.wall@gmail.com>
2022-09-08 17:08:31 -0700

    Add price to available contracts and restyle

--- src/components/ui/Cards/AvailableContracts.tsx
+++ src/components/ui/Cards/AvailableContracts.tsx
@@ -1,11 +1,15 @@
 import { HashRentalContract } from '../../../types';
 import { getReadableDate } from '../../../utils';
+import SpeedIcon from '../../../images/icons/download-speed.png';
+import PriceIcon from '../../../images/icons/price-tag.png';
+import TimeIcon from '../../../images/icons/time-left.png';
+import { AvailableContract } from './AvailableContract.styled';
 
 export const AvailableContracts = (prop: { contracts: Array<HashRentalContract> }) => {
 	return (
 		<div className='overflow-visible w-full'>
 			{prop.contracts.map((item, index) => (
-				<div className='bg-white rounded-15 mb-3 p-7 w-full flex flex-row justify-between items-center'>
+				<AvailableContract>
 					<a
 						className='underline pb-0 font-Raleway cursor-pointer'
 						href={`https://ropsten.etherscan.io/address/${item.contractId}`}
@@ -14,10 +18,22 @@ export const AvailableContracts = (prop: { contracts: Array<HashRentalContract> 
 					>
 						View Contract
 					</a>
-					<p className='pb-0'>{item.speed} th/s</p>
-					{item.length && <p className='pb-0'>{getReadableDate(item.length)}</p>}
+					<p>
+						<img src={SpeedIcon} alt='' />
+						{item.speed} th/s
+					</p>
+					{item.length && (
+						<p>
+							<img src={TimeIcon} alt='' />
+							{getReadableDate(item.length)}
+						</p>
+					)}
+					<p>
+						<img src={PriceIcon} alt='' />
+						{item.price} LMR
+					</p>
 					{item.trade}
-				</div>
+				</AvailableContract>
 			))}
 		</div>
 	);
