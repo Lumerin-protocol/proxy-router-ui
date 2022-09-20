3f4cdae7f6347bb6763cb36eb6bdac2e2059aac5
codywall <cody.wall@gmail.com>
2022-09-20 13:54:28 -0700

    Change nav item icon color based on current page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,25 +3,21 @@ import { Fragment, Suspense, useEffect, useState } from 'react';
 import { Link, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import { Dialog, Transition } from '@headlessui/react';
 import { MenuAlt2Icon, XIcon } from '@heroicons/react/outline';
-import {
-	ContractIcon,
-	MarketplaceIcon,
-	MyOrdersIcon,
-	MetaMaskIcon,
-	LogoIcon,
-	LogoIcon2,
-	LumerinIcon,
-	LumerinLandingPage,
-	WalletConnectIcon,
-} from '../images/index';
+import { MetaMaskIcon, LogoIcon, LogoIcon2, LumerinIcon, WalletConnectIcon } from '../images/index';
 import BubbleGraphic1 from '../images/Bubble_1.png';
 import BubbleGraphic2 from '../images/Bubble_2.png';
 import BubbleGraphic3 from '../images/Bubble_3.png';
 import BubbleGraphic4 from '../images/Bubble_4.png';
+import MarketplaceIconActive from '../images/icons/store-blue.png';
+import MarketplaceIconInactive from '../images/icons/store-grey.png';
+import BuyerIconActive from '../images/icons/buyer-blue.png';
+import BuyerIconInactive from '../images/icons/buyer-grey.png';
+import SellerIconActive from '../images/icons/seller-blue.png';
+import SellerIconInactive from '../images/icons/seller-grey.png';
 import ImplementationContract from '../contracts/Implementation.json';
 import { AbiItem } from 'web3-utils';
 import { Alert } from './ui/Alert';
-import { Modal } from './ui/Modal';
+import { ModalItem } from './ui/Modal';
 import { Marketplace } from './Marketplace';
 import { Contract } from 'web3-eth-contract';
 import { BuyForm } from './ui/Forms/BuyerForms/BuyForm';
@@ -90,7 +86,8 @@ export const Main: React.FC = () => {
 	interface Navigation {
 		name: string;
 		to: string;
-		icon: JSX.Element;
+		activeIcon: string;
+		inactiveIcon: string;
 		current: boolean;
 	}
 	const pathName = window.location.pathname;
@@ -98,19 +95,22 @@ export const Main: React.FC = () => {
 		{
 			name: 'Marketplace',
 			to: PathName.Marketplace,
-			icon: <MarketplaceIcon />,
+			activeIcon: MarketplaceIconActive,
+			inactiveIcon: MarketplaceIconInactive,
 			current: pathName === PathName.Marketplace,
 		},
 		{
 			name: 'Buyer Hub',
 			to: PathName.MyOrders,
-			icon: <MyOrdersIcon />,
+			activeIcon: BuyerIconActive,
+			inactiveIcon: BuyerIconInactive,
 			current: pathName === PathName.MyOrders,
 		},
 		{
 			name: 'Seller Hub',
 			to: PathName.MyContracts,
-			icon: <ContractIcon />,
+			activeIcon: SellerIconActive,
+			inactiveIcon: SellerIconInactive,
 			current: pathName === PathName.MyContracts,
 		},
 	];
@@ -287,11 +287,7 @@ export const Main: React.FC = () => {
 				<span className='mr-4'>{WalletText.ConnectViaMetaMask}</span>
 				<MetaMaskIcon />
 			</button>
-			<button
-				type='button'
-				className='btn-wallet w-60 h-12 mt-4 rounded-15 text-lumerin-dark-blue text-sm font-Inter'
-				onClick={() => connectWallet(WalletText.ConnectViaWalletConnect)}
-			>
+			<button type='button' onClick={() => connectWallet(WalletText.ConnectViaWalletConnect)}>
 				<span className='mr-4'>{WalletText.ConnectViaWalletConnect}</span>
 				<WalletConnectIcon />
 			</button>
@@ -401,6 +397,16 @@ export const Main: React.FC = () => {
 		background-size: 25% 15% 15% 10%;
 	`;
 
+	const ActiveNavTab = styled.div`
+		background: #0e4353;
+		position: absolute;
+		width: 6px;
+		height: 36px;
+		left: 0px;
+		margin-top: 0.2rem;
+		border-radius: 0px 15px 15px 0px;
+	`;
+
 	return isConnected ? (
 		<BodyWrapper>
 			<Alert
@@ -409,7 +415,7 @@ export const Main: React.FC = () => {
 				setOpen={setAlertOpen}
 				onClick={isMetaMask ? changeNetworkAsync : () => {}}
 			/>
-			<Modal
+			<ModalItem
 				open={buyModalOpen}
 				setOpen={setBuyModalOpen}
 				content={
@@ -424,7 +430,7 @@ export const Main: React.FC = () => {
 					/>
 				}
 			/>
-			<Modal
+			<ModalItem
 				open={createModalOpen}
 				setOpen={setCreateModalOpen}
 				content={
@@ -436,7 +442,7 @@ export const Main: React.FC = () => {
 					/>
 				}
 			/>
-			<Modal
+			<ModalItem
 				open={sellerEditModalOpen}
 				setOpen={setSellerEditModalOpen}
 				content={
@@ -449,7 +455,7 @@ export const Main: React.FC = () => {
 					/>
 				}
 			/>
-			<Modal
+			<ModalItem
 				open={buyerEditModalOpen}
 				setOpen={setBuyerEditModalOpen}
 				content={
@@ -462,7 +468,7 @@ export const Main: React.FC = () => {
 					/>
 				}
 			/>
-			<Modal
+			<ModalItem
 				open={cancelModalOpen}
 				setOpen={setCancelModalOpen}
 				content={
@@ -475,7 +481,7 @@ export const Main: React.FC = () => {
 					/>
 				}
 			/>
-			<Modal
+			<ModalItem
 				open={claimLmrModalOpen}
 				setOpen={setClaimLmrModalOpen}
 				content={
@@ -547,13 +553,13 @@ export const Main: React.FC = () => {
 											to={item.to}
 											className={classNames(
 												item.current ? 'text-lumerin-dark-blue' : 'text-lumerin-black-text',
-												'flex items-center px-2 py-2 text-sm font-medium rounded-md'
+												'flex items-center px-2 py-2 mb-2 text-sm font-medium rounded-md'
 											)}
 											onClick={() => {
 												setToggle(!toggle);
 											}}
 										>
-											{item.icon}
+											<img src={item.current ? item.activeIcon : item.inactiveIcon} alt='' />
 											<span className='ml-4'>{item.name}</span>
 										</Link>
 									))}
@@ -590,18 +596,25 @@ export const Main: React.FC = () => {
 						<div className='flex-1 flex flex-col'>
 							<nav className='flex-1 px-2 space-y-1'>
 								{navigation.map((item) => (
-									<Link
-										key={item.name}
-										to={item.to}
-										className={classNames(
-											item.current ? 'text-lumerin-blue-text' : 'text-lumerin-inactive-text',
-											'flex items-center px-2 py-2 text-sm font-medium rounded-md'
-										)}
-										onClick={() => setToggle(!toggle)}
-									>
-										{item.icon}
-										<span className='ml-4'>{item.name}</span>
-									</Link>
+									<>
+										{item.current && <ActiveNavTab />}
+										<Link
+											key={item.name}
+											to={item.to}
+											className={classNames(
+												'flex items-center pl-6 py-2 pb-6 text-sm font-medium rounded-md',
+												item.current ? 'text-lumerin-blue-text' : 'text-lumerin-inactive-text'
+											)}
+											onClick={() => setToggle(!toggle)}
+										>
+											<img
+												className='w-5'
+												src={item.current ? item.activeIcon : item.inactiveIcon}
+												alt=''
+											/>
+											<span className='text-sm ml-4'>{item.name}</span>
+										</Link>
+									</>
 								))}
 							</nav>
 						</div>
