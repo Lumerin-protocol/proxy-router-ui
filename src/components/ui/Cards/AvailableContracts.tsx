22774c8f686af2cc36b7005b95fc616bdf32b249
codywall <cody.wall@gmail.com>
2022-09-21 12:29:53 -0700

    Refactor loading, use skeletons instead of spinner

--- src/components/ui/Cards/AvailableContracts.tsx
+++ src/components/ui/Cards/AvailableContracts.tsx
@@ -3,40 +3,60 @@ import { getReadableDate } from '../../../utils';
 import SpeedIcon from '../../../images/icons/download-speed.png';
 import PriceIcon from '../../../images/icons/price-tag.png';
 import TimeIcon from '../../../images/icons/time-left.png';
-import { AvailableContract } from './AvailableContract.styled';
+import { AvailableContract, SkeletonWrap } from './AvailableContract.styled';
+import { Skeleton } from '@mui/material';
 
-export const AvailableContracts = (prop: { contracts: Array<HashRentalContract> }) => {
+export const AvailableContracts = (prop: {
+	contracts: Array<HashRentalContract>;
+	loading: boolean;
+}) => {
 	return (
-		<div className='overflow-visible w-full'>
-			{prop.contracts.map((item, index) => (
-				<AvailableContract>
-					<p>
-						<a
-							className='underline pb-0 font-Raleway cursor-pointer'
-							href={`https://ropsten.etherscan.io/address/${item.contractId}`}
-							target='_blank'
-							rel='noreferrer'
-						>
-							View Contract
-						</a>
-					</p>
-					<p>
-						<img src={SpeedIcon} alt='' />
-						{item.speed} th/s
-					</p>
-					{item.length && (
-						<p>
-							<img src={TimeIcon} alt='' />
-							{getReadableDate(item.length)}
-						</p>
+		<>
+			{prop.loading ? (
+				[...Array(10)].map((elementInArray, index) => (
+					<SkeletonWrap>
+						<Skeleton variant='rectangular' width={'100%'} height={100} />
+					</SkeletonWrap>
+				))
+			) : (
+				<>
+					{prop.contracts.length > 0 ? (
+						<ul>
+							{prop.contracts.map((item, index) => (
+								<AvailableContract>
+									<p>
+										<a
+											className='underline pb-0 font-Raleway cursor-pointer'
+											href={`https://ropsten.etherscan.io/address/${item.contractId}`}
+											target='_blank'
+											rel='noreferrer'
+										>
+											View Contract
+										</a>
+									</p>
+									<p>
+										<img src={SpeedIcon} alt='' />
+										{item.speed} th/s
+									</p>
+									{item.length && (
+										<p>
+											<img src={TimeIcon} alt='' />
+											{getReadableDate(item.length)}
+										</p>
+									)}
+									<p>
+										<img src={PriceIcon} alt='' />
+										{item.price} LMR
+									</p>
+									{item.trade}
+								</AvailableContract>
+							))}
+						</ul>
+					) : (
+						<h2>No contracts available for purchase.</h2>
 					)}
-					<p>
-						<img src={PriceIcon} alt='' />
-						{item.price} LMR
-					</p>
-					{item.trade}
-				</AvailableContract>
-			))}
-		</div>
+				</>
+			)}
+		</>
 	);
 };
