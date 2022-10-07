e352376eccb4ae49b0c6339718f494b61bf1a791
codywall <cody.wall@gmail.com>
2022-10-07 15:23:26 -0700

    Remove grow animation from contracts

--- src/components/ui/Cards/AvailableContracts.tsx
+++ src/components/ui/Cards/AvailableContracts.tsx
@@ -4,7 +4,7 @@ import SpeedIcon from '../../../images/icons/download-speed.png';
 import PriceIcon from '../../../images/icons/price-tag.png';
 import TimeIcon from '../../../images/icons/time-left.png';
 import { AvailableContract, SkeletonWrap } from './AvailableContract.styled';
-import { Fade, Grow, Skeleton, Slide } from '@mui/material';
+import { Skeleton } from '@mui/material';
 
 export const AvailableContracts = (prop: {
 	contracts: Array<HashRentalContract>;
@@ -15,7 +15,7 @@ export const AvailableContracts = (prop: {
 			{prop.loading ? (
 				[...Array(10)].map((elementInArray, index) => (
 					<SkeletonWrap>
-						<Skeleton variant='rectangular' width={'100%'} height={100} />
+						<Skeleton variant='rectangular' width={'100%'} height={100} key={index} />
 					</SkeletonWrap>
 				))
 			) : (
@@ -23,35 +23,33 @@ export const AvailableContracts = (prop: {
 					{prop.contracts.length > 0 ? (
 						<ul>
 							{prop.contracts.map((item, index) => (
-								<Grow in={true}>
-									<AvailableContract key={index}>
+								<AvailableContract key={index}>
+									<p>
+										<a
+											className='underline pb-0 font-Raleway cursor-pointer'
+											href={`https://goerli.etherscan.io/address/${item.contractId}`}
+											target='_blank'
+											rel='noreferrer'
+										>
+											View Contract
+										</a>
+									</p>
+									<p>
+										<img src={SpeedIcon} alt='' />
+										{item.speed} th/s
+									</p>
+									{item.length && (
 										<p>
-											<a
-												className='underline pb-0 font-Raleway cursor-pointer'
-												href={`https://goerli.etherscan.io/address/${item.contractId}`}
-												target='_blank'
-												rel='noreferrer'
-											>
-												View Contract
-											</a>
+											<img src={TimeIcon} alt='' />
+											{getReadableDate(item.length.toString())}
 										</p>
-										<p>
-											<img src={SpeedIcon} alt='' />
-											{item.speed} th/s
-										</p>
-										{item.length && (
-											<p>
-												<img src={TimeIcon} alt='' />
-												{getReadableDate(item.length)}
-											</p>
-										)}
-										<p>
-											<img src={PriceIcon} alt='' />
-											{item.price} LMR
-										</p>
-										{item.trade}
-									</AvailableContract>
-								</Grow>
+									)}
+									<p>
+										<img src={PriceIcon} alt='' />
+										{item.price} LMR
+									</p>
+									{item.trade}
+								</AvailableContract>
 							))}
 						</ul>
 					) : (
