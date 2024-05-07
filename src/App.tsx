084e77bc459de5b69b5eb9cee99efb71085b7c7c
codywall <cody.wall@gmail.com>
2024-05-07 14:56:44 -0700

    Add dark theme to MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Main } from './components/Main';
 import ReactGA from 'react-ga4';
+import { ThemeProvider } from '@mui/material';
+import { darkTheme } from '../styles/themeOptions';
 
 const trackingId = 'G-TN08K48RMS';
 ReactGA.initialize(trackingId);
 
-export const App: React.FC = () => <Main />;
+export const App: React.FC = () => (
+	<ThemeProvider theme={darkTheme}>
+		<Main />
+	</ThemeProvider>
+);
 App.displayName = 'App';
 App.whyDidYouRender = false;
