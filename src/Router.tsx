606a251cb6b019fe76b3a400e5f4a05bae77ffb1
Oleksandr (Shev) Shevchuk <shev@titan.io>
2025-06-11 17:28:13 +0200

    fix: safe lazy load

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,27 @@ import { type FC, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router";
 import { Spinner } from "./components/Spinner.styled";
 import { PathName } from "./types/types";
-// import { Test } from "./pages/test/test";
+import { safeLazy } from "react-safe-lazy";
+
+// biome-ignore lint/complexity/noBannedTypes: invalid typings for safeLazy
+type Empty = {};
 
 // Lazy loaded components
-const BuyerHub = lazy(() => import("./pages/buyer-hub/BuyerHub").then((module) => ({ default: module.BuyerHub })));
-const Landing = lazy(() => import("./pages/landing/Landing").then((module) => ({ default: module.Landing })));
-const Marketplace = lazy(() =>
+// SafeLazy is a wrapper around lazy that will reload the html page if the component fails to load
+// the chunks, likely due to changed js asset filename
+const BuyerHub = safeLazy<Empty>(() =>
+  import("./pages/buyer-hub/BuyerHub").then((module) => ({ default: module.BuyerHub })),
+);
+const Landing = safeLazy<Empty>(() =>
+  import("./pages/landing/Landing").then((module) => ({ default: module.Landing })),
+);
+const Marketplace = safeLazy<Empty>(() =>
   import("./pages/marketplace/Marketplace").then((module) => ({ default: module.Marketplace })),
 );
-const SellerHub = lazy(() => import("./pages/seller-hub/SellerHub").then((module) => ({ default: module.SellerHub })));
-const ValidatorHub = lazy(() =>
+const SellerHub = safeLazy<Empty>(() =>
+  import("./pages/seller-hub/SellerHub").then((module) => ({ default: module.SellerHub })),
+);
+const ValidatorHub = safeLazy<Empty>(() =>
   import("./pages/validator-hub/ValidatorHub").then((module) => ({ default: module.ValidatorHub })),
 );
 
