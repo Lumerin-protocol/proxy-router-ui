85e37a5d498255da512574bbc2cf8b2a12e3f4e9
Oleksandr (Shev) Shevchuk <shev@titan.io>
2025-06-16 15:58:18 +0200

    fix: enable modal scroll for small vertical height in some mobile browsers when keyboard is opened

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,17 +2,26 @@ import CloseIcon from "@mui/icons-material/Close";
 import IconButton from "@mui/material/IconButton";
 import Modal from "@mui/material/Modal";
 import { ModalCard } from "./Modal.styled";
+import type { FC, ReactNode } from "react";
+import { css } from "@mui/material/styles";
 
 interface ModalProps {
   open: boolean;
   setOpen: (isOpen: boolean) => void;
-  content?: React.ReactNode;
-  children?: React.ReactNode;
+  content?: ReactNode;
+  children?: ReactNode;
 }
 
-export const ModalItem: React.FC<ModalProps> = ({ open, setOpen, content, children }) => {
+export const ModalItem: FC<ModalProps> = ({ open, setOpen, content, children }) => {
   return (
-    <Modal open={open} onClose={() => setOpen}>
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      css={css`
+        position: absolute;
+        overflow: scroll;
+      `}
+    >
       <ModalCard>
         <IconButton className="close" sx={{ color: "white" }} onClick={() => setOpen(false)}>
           <CloseIcon />
@@ -22,5 +31,3 @@ export const ModalItem: React.FC<ModalProps> = ({ open, setOpen, content, childr
     </Modal>
   );
 };
-
-ModalItem.displayName = "Modal";
